fad22177361f2151868b50978d9c6285b8f215bb
DESKTOP-EBE9M9V\mathi <mathijs.kemp@eproseed.com>
2018-06-28 10:03:01 +0200

    .

--- multiSourceBootstrap/js/main/main.js
+++ multiSourceBootstrap/js/main/main.js
@@ -67,6 +67,9 @@ function setInfo(input) {
 var slider = document.getElementById("myRange");
 slider.oninput = function () {
     if (this.value === '15' || this.value === '1') {
+//        var source = map.getSource('points0');
+//        console.log("source: " + source);
+//        map.removeSource(source);
         removePoints();
         drawRandomPoints(1);
     }
