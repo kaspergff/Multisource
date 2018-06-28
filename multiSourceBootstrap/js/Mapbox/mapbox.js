2473b0b1fa5d407f19879ec999d7e80a9b839409
DESKTOP-EBE9M9V\mathi <mathijs.kemp@eproseed.com>
2018-06-28 10:08:55 +0200

    syntax fix voor kaart

--- multiSourceBootstrap/js/Mapbox/mapbox.js
+++ multiSourceBootstrap/js/Mapbox/mapbox.js
@@ -289,7 +289,7 @@ function mh17Icons(){
         .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
         .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
         .addTo(map);
-    }
+    });
 }
 
 
